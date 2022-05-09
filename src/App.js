789012e4473e906b6518ff0d603a1ceea0bad779
Sumit Verma <sumeet0604.sv@gmail.com>
2022-05-10 01:05:54 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,20 +7,38 @@ import NavBar from "./components/NavBar";
 import Offer from "./components/Offer";
 import Projects from "./components/Projects";
 import Services from "./components/Services";
+import Loader from "./components/Loader";
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <div className="bg-[#1C0C5B]">
-        <NavBar />
-        <Header />
-        <About />
-        <Services />
-        <Projects />
-        <Offer />
-        <Contact />
-        <Footer />
-      </div>
+      {
+        loading ?
+          <div className="flex justify-center items-center top-1/2">
+            <Loader />
+          </div>
+          :
+          <div className="bg-[#1C0C5B]">
+            <NavBar />
+            <Header />
+            <About />
+            <Services />
+            <Projects />
+            <Offer />
+            <Contact />
+            <Footer />
+          </div>
+      }
     </>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <>
+            <div className="spinner">
+                <span>L</span>
+                <span>O</span>
+                <span>A</span>
+                <span>D</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+            </div>
+        </>
+    )
+}
+
+export default Loader
