2bf132df84264c53e57fb0f783d6ad16786cbf32
Sumit Verma <sumeet0604.sv@gmail.com>
2022-05-10 15:07:49 +0530

    some more animation added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import { FaTwitter, FaInstagramSquare } from 'react-icons/fa'
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Tada from 'react-reveal/Tada';
 import LightSpeed from 'react-reveal/LightSpeed';
-import HeadShake from 'react-reveal/HeadShake';
+// import HeadShake from 'react-reveal/HeadShake';
+import Fade from 'react-reveal/Fade';
 
 
 const Header = () => {
@@ -42,9 +43,11 @@ const Header = () => {
                                 />
                             </div>
 
-                            <div className='text-center text-gray-500 px-2 pb-4 '>
-                                <p>Innovative Front End Developer with good experience in maintaining and building web pages. I like to craft solid and scalable frontend products with great user experiences.</p>
-                            </div>
+                            <Fade left big>
+                                <div className='text-center text-gray-500 px-2 pb-4 '>
+                                    <p>Innovative Front End Developer with good experience in maintaining and building web pages. I like to craft solid and scalable frontend products with great user experiences.</p>
+                                </div>
+                            </Fade>
                             <div className='flex flex-col text-gray-500'>
                                 <LightSpeed left>
                                     <button className='header-button'>
@@ -66,21 +69,32 @@ const Header = () => {
                     {/* Social Media  */}
                     <div className='py-6 px-8 max-w-[300px] m-auto flex justify-center flex-shrink-0 items-center space-x-16 text-gray-300'>
                         {/* Github */}
-                        <a href="https://github.com/SUM-X-0604" target="_blank" rel="noreferrer">
-                            <AiFillGithub size={20} />
-                        </a>
+                        <Fade bottom big>
+                            <a href="https://github.com/SUM-X-0604" target="_blank" rel="noreferrer">
+                                <AiFillGithub size={20} />
+                            </a>
+                        </Fade>
                         {/* Twitter */}
-                        <a href="https://twitter.com/_Fire_hawk" target="_blank" rel="noreferrer">
-                            <FaTwitter size={20} />
-                        </a>
+                        <Fade top big>
+                            <a href="https://twitter.com/_Fire_hawk" target="_blank" rel="noreferrer">
+                                <FaTwitter size={20} />
+                            </a>
+                        </Fade>
                         {/* Linkedin */}
-                        <a href="https://www.linkedin.com/in/sumit-verma-07835a210/" target="_blank" rel="noreferrer">
-                            <AiFillLinkedin size={20} />
-                        </a>
+                        <Fade bottom big>
+
+                            <a href="https://www.linkedin.com/in/sumit-verma-07835a210/" target="_blank" rel="noreferrer">
+                                <AiFillLinkedin size={20} />
+                            </a>
+                        </Fade>
+
                         {/* instagram */}
-                        <a href="https://www.instagram.com/__fire_hawk/" target="_blank" rel="noreferrer">
-                            <FaInstagramSquare size={20} />
-                        </a>
+                        <Fade top big>
+
+                            <a href="https://www.instagram.com/__fire_hawk/" target="_blank" rel="noreferrer">
+                                <FaInstagramSquare size={20} />
+                            </a>
+                        </Fade>
                     </div>
                     {/* Social Media end */}
                 </div>
