009877a5d5e2dcead433b020e2945af39441723b
Sumit Verma <sumeet0604.sv@gmail.com>
2022-05-11 02:14:30 +0530

    contact animation added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,11 @@ import { UilPhone } from '@iconscout/react-unicons';
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsWhatsapp } from 'react-icons/bs'
 import { GoLocation } from 'react-icons/go'
-// import Fade from 'react-reveal/Fade';
+import Fade from 'react-reveal/Fade';
+
 
 const Contact = () => {
+
 
     return (
         <div className='max-w-[1240px] m-auto pt-16 pb-8 lg:pt-24 ' id='contact'>
@@ -21,58 +23,58 @@ const Contact = () => {
 
                 {/* contact links 1st */}
                 <div className='flex items-center px-4 pt-8'>
-                    {/* <Fade left> */}
-                    <div className='pr-4' >
-                        <UilPhone className="h-8 w-10 text-blue-700" />
-                    </div>
-                    <div className='leading-6' >
-                        <h1 className='text-xl font-bold text-gray-300'>Call Me</h1>
-                        <p className='text-gray-500'>7999001618</p>
-                    </div>
-                    {/* </Fade> */}
+                    <Fade left>
+                        <div className='pr-4' >
+                            <UilPhone className="h-8 w-10 text-blue-700" />
+                        </div>
+                        <div className='leading-6' >
+                            <h1 className='text-xl font-bold text-gray-300'>Call Me</h1>
+                            <p className='text-gray-500'>7999001618</p>
+                        </div>
+                    </Fade>
                 </div>
                 {/*end contact links 1st */}
 
                 {/* contact links 2nd */}
                 <div className='flex items-center px-4 pt-8'>
-                    {/* <Fade right> */}
-                    <div className='pr-4' >
-                        <AiOutlineMail className="h-8 w-10 text-blue-700" />
-                    </div>
-                    <div className='leading-6' >
-                        <h1 className='text-xl font-bold text-gray-300'>Email</h1>
-                        <p className='text-gray-500'>sumeet0604.sv@gmail.com</p>
-                    </div>
-                    {/* </Fade> */}
+                    <Fade right>
+                        <div className='pr-4' >
+                            <AiOutlineMail className="h-8 w-10 text-blue-700" />
+                        </div>
+                        <div className='leading-6' >
+                            <h1 className='text-xl font-bold text-gray-300'>Email</h1>
+                            <p className='text-gray-500'>sumeet0604.sv@gmail.com</p>
+                        </div>
+                    </Fade>
                 </div>
                 {/*end contact links 2nd */}
 
                 {/* contact links 3rd */}
                 <div className='flex items-center px-4 pt-8'>
-                    {/* <Fade left> */}
+                    <Fade left>
 
-                    <div className='pr-4' >
-                        <BsWhatsapp className="h-8 w-10 text-blue-700" />
-                    </div>
-                    <div className='leading-6' >
-                        <h1 className='text-xl font-bold text-gray-300'>Whatsapp</h1>
-                        <p className='text-gray-500'>7999001618</p>
-                    </div>
-                    {/* </Fade> */}
+                        <div className='pr-4' >
+                            <BsWhatsapp className="h-8 w-10 text-blue-700" />
+                        </div>
+                        <div className='leading-6' >
+                            <h1 className='text-xl font-bold text-gray-300'>Whatsapp</h1>
+                            <p className='text-gray-500'>7999001618</p>
+                        </div>
+                    </Fade>
                 </div>
                 {/*end contact links 3rd */}
 
                 {/* contact links 4th */}
                 <div className='flex items-center px-4 pt-8'>
-                    {/* <Fade right> */}
-                    <div className='pr-4' >
-                        <GoLocation className="h-8 w-10 text-blue-700" />
-                    </div>
-                    <div className='leading-6' >
-                        <h1 className='text-xl font-bold text-gray-300'>Location</h1>
-                        <p className='text-gray-500'>Raipur(CG), India</p>
-                    </div>
-                    {/* </Fade> */}
+                    <Fade right>
+                        <div className='pr-4' >
+                            <GoLocation className="h-8 w-10 text-blue-700" />
+                        </div>
+                        <div className='leading-6' >
+                            <h1 className='text-xl font-bold text-gray-300'>Location</h1>
+                            <p className='text-gray-500'>Raipur(CG), India</p>
+                        </div>
+                    </Fade>
                 </div>
                 {/*end contact links 4th */}
 
