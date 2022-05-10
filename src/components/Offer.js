4debe237c40a25e4c773e872d724d39862adf7c2
Sumit Verma <sumeet0604.sv@gmail.com>
2022-05-11 01:57:29 +0530

    bug fixed

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import project from '../image/newproject.png';
-import Fade from 'react-reveal/Fade';
+// import Fade from 'react-reveal/Fade';
 
 
 const Offer = () => {
     return (
         <div className='h-full max-w-[1240px] m-auto rounded-lg bg-[#070047]' >
-            <div className='flex flex-col items-center justify-center px-4 m-auto py-8 flex-wrap md:flex-row md:justify-around'>
+            <div className='flex flex-col items-center justify-center px-4 m-auto py-8 flex-wrap md:flex-row md:justify-evenly'>
                 {/* left */}
                 <div className='flex flex-col justify-center items-center '>
-                    <Fade left>
-                        <h1 className='text-gray-400 font-bold text-xl md:text-3xl'>You Have a new project?</h1>
-                        <p className='text-gray-500 font-semibold py-2 md:text-lg'>Contact me now and get a huge discount on your new project.</p>
-                    </Fade>
+
+                    <h1 className='text-gray-400 font-bold text-xl md:text-3xl'>You Have a new project?</h1>
+                    <p className='text-gray-500 font-semibold py-2 md:text-lg'>Contact me now and get a huge discount on your new project.</p>
+
                 </div>
                 {/* right */}
                 <div className='pt-4'>
-                    <Fade right>
-                        <img src={project} alt="" className='h-48 w-60' />
-                    </Fade>
+
+                    <img src={project} alt="" className='h-48 w-60' />
+
                 </div>
 
             </div>
