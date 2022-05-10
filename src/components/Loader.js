9ad353d2a5ef1cbab7872ed3ea63458fcca52ea3
Sumit Verma <sumeet0604.sv@gmail.com>
2022-05-11 02:09:23 +0530

    loader updated

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,13 +4,11 @@ const Loader = () => {
     return (
         <>
             <div className="spinner">
-                <span>L</span>
-                <span>O</span>
-                <span>A</span>
-                <span>D</span>
+                <span>S</span>
+                <span>U</span>
+                <span>M</span>
                 <span>I</span>
-                <span>N</span>
-                <span>G</span>
+                <span>T</span>
             </div>
         </>
     )
